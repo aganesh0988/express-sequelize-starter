86aeffb8d870e418ae7affb2b9526490dd7b9ba4
Andrea <aganesh0988@gmail.com>
2020-09-01 20:40:33 -0500

    adding tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,9 +4,15 @@ const db = require("../db/models");
 
 const { Task } = db;
 
-router.get("/", (req, res) => {
-  // TODO: Fetch all tasks from the database (Read)
-  // TODO: Render all tasks in JSON format
-});
+const asyncHandler = (handler) => (req, res, next) =>
+  handler(req, res, next).catch(next);
+
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const tasks = await Task.findAll();
+    res.json({ tasks });
+  })
+);
 
 module.exports = router;
